6ae651867a8280d99e930003e14ad089fd239311
sravan reddy <sravanreddy234@gmail.com>
2016-11-18 23:01:34 +0530

    70%

---
+++ src/main/java/com/niit/model/Message.java
@@ -0,0 +1,17 @@
+package com.niit.model;
+
+public class Message {
+	
+	private String message;
+	private String name;
+	
+	private int id;
+	
+	public Message(){
+		
+	}
+	
+	public Message(int id, String message){
+		this.id = id;
+	}
+}
